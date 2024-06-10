5f1d8e754729608b59a160fdcf115de3cc8b1d87
jhonatan-note-ubuntu <jhonatan.oliveira.dev@gmail.com>
2024-06-10 17:31:45 -0300

    Att

--- wp-web/06-servelet/AloMundo/src/java/servelets/alou.java
+++ wp-web/06-servelet/AloMundo/src/java/servelets/alou.java
@@ -39,7 +39,7 @@ public class alou extends HttpServlet {
             out.println("<title>Servlet alou</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet alou at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet alou at " + request.getContextPath() + " funcionando "+"</h1>");
             out.println("</body>");
             out.println("</html>");
         }
